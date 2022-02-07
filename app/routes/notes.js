6b6cc61fa8bbb3d0cc21701316113d87ce2c6f1c
Diego <diegorf55@gmail.com>
2022-02-07 18:06:15 -0300

    download a note

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -2,7 +2,8 @@ var express = require('express');
 const Note = require('../models/note');
 var router = express.Router();
 const User = require('../models/note');
-const WithAuth = require('../middlewares/auth')
+const WithAuth = require('../middlewares/auth');
+const note = require('../models/note');
 
 router.post('/', WithAuth, async (req, res) => {
     const { title, body } = req.body;
@@ -17,5 +18,25 @@ router.post('/', WithAuth, async (req, res) => {
         res.status(500).json({error: 'Problem to create a new note'})
     }
 })
+
+router.get('/:id', WithAuth, async (req, res) => {
+    try {
+        const { id } = req.params;
+        let note = await Note.findById(id)
+        if(isOwner(req.user, note))
+            res.json(note)
+        else
+            res.status(403).json({error: 'Permission denied'})
+    } catch (error) {
+        res.status(500).json({error: 'Problem to get a note!'})
+    }
+})
+
+const isOwner = (user, note) => {
+    if(JSON.stringify(user._id) == JSON.stringify(note.author._id))
+        return true;
+    else
+        return false;
+}
 
 module.exports = router;
